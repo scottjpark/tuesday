8d70bc71aaa1a4daeab620be0edd2a2c060e3524
Scott <scott@scottjpark.com>
2023-07-08 16:00:43 -0400

    Finish user auth endpoints, remove pycache

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from .views import RegisterView
+from .views import RegisterView, RetrieveUserView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name ='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name ='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name ='token_verify'),
-    path('register', RegisterView.as_view())
+    path('register/', RegisterView.as_view()),
+    path('profile/', RetrieveUserView.as_view())
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,24 +1,26 @@
 from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-from .serializers import UserCreateSerializer
+from .serializers import UserCreateSerializer, UserSerializer
 
 class RegisterView(APIView):
     def post(self, request):
         data = request.data
-        username = data['username']
-        email = data['email']
-        password = data['password']
 
-        user = User.objects.create_user(username, email, password)
-        user = UserCreateSerializer(user)
+        serializer = UserCreateSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
 
         return Response(user.data, status=status.HTTP_201_CREATED)
 
 class RetrieveUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
-        pass
+        user = request.user
+        user = UserSerializer(user)
+
+        return Response(user.data, status=status.HTTP_200_OK)
