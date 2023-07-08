49056fcca4639978dc2e6a98a8467b35c81f7280
Scott Park <49740546+scottjpark@users.noreply.github.com>
2023-07-08 16:02:19 -0400

    Rework Auth Backend
    * Reworking backend
    
    * Reworking backend still
    
    * Finish user auth endpoints, remove pycache

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,22 +1,26 @@
+from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import status
+from .serializers import UserCreateSerializer, UserSerializer
 
-class ProfileView(APIView):
-    permission_classes = (IsAuthenticated,)
-    def get(self, request):
-        content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
-        return Response(content)
-
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
+class RegisterView(APIView):
     def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        data = request.data
+
+        serializer = UserCreateSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
+
+        return Response(user.data, status=status.HTTP_201_CREATED)
+
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        user = request.user
+        user = UserSerializer(user)
+
+        return Response(user.data, status=status.HTTP_200_OK)
--- frontend/src/pages/auth/login.js
+++ frontend/src/pages/auth/login.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = {
+            username: username,
+            password: password
+        }
+        console.log(user)
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <form id='auth'>
+            <label>Username</label>
+            <input
+                className='auth-input'
+                placeholder='Username'
+                name='username'
+                type='text'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+                className='auth-input'
+                placeholder='Password'
+                name='password'
+                type='password'
+                value={username}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button type='submit' onClick={handleSubmit}>Submit</button>
+        </form>
     )
 }
